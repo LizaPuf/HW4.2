22985a9af6b64646cae28d5679693cce06881149
Elizaveta Slonova <pufliza@gmail.com>
2022-12-22 19:39:25 +0300

    refactor code

--- main.py
+++ main.py
@@ -3,29 +3,21 @@ from pprint import pprint
 import csv
 
 
-PHONE_REGEX = r"[7|8]\s?\(?(?P<area>\d{3})\)?[\- ]?(?P<number>[\d\- ]{7,10})(?P<ext>\(?доб. \d{4}?\)?)?"
-
-employees = {}
-
-with open("phonebook_raw.csv") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-for line in contacts_list[1:]:
-    print(line)
-    lastname, firstname, surname, organization, position, phone, email, *_ = line
-    print(position)
-    phone_number=''
-    match =  re.search(PHONE_REGEX, phone)
+def normalize_phone_number(phone):
+    phone_number = ''
+    match = re.search(PHONE_REGEX, phone)
     if match:
         area_code = match.group('area')
-        number = match.group('number').strip().replace('-','')
+        number = match.group('number').strip().replace('-', '')
         number = f"{number[0:3]}-{number[3:5]}-{number[5:]}"
         extension = match.group('ext') or ''
         extension = re.sub(r'[\(\)\s]?', '', extension)
-        phone_number = f"+7({area_code}){number}{' '*bool(extension)}{extension}"
-        '''
-        print(phone_number)
-        '''
+        phone_number = f"+7({area_code}){number}{' ' * bool(extension)}{extension}"
+
+    return phone_number
+
+
+def normalize_names(lastname, firstname, surname):
     if not all([lastname, firstname, surname]):
         if not surname and ' ' in firstname:
             firstname, surname = firstname.split(' ')
@@ -35,7 +27,20 @@ for line in contacts_list[1:]:
             except ValueError:
                 lastname, firstname = lastname.split(' ')
 
-    print(lastname, firstname, surname, sep=',')
+    return lastname, firstname, surname
+
+PHONE_REGEX = r"[7|8]\s?\(?(?P<area>\d{3})\)?[\- ]?(?P<number>[\d\- ]{7,10})(?P<ext>\(?доб. \d{4}?\)?)?"
+
+employees = {}
+
+with open("phonebook_raw.csv") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+
+for line in contacts_list[1:]:
+    lastname, firstname, surname, organization, position, phone, email, *_ = line
+    phone_number = normalize_phone_number(phone)
+    lastname, firstname, surname = normalize_names(lastname, firstname, surname)
 
     if (lastname, firstname) not in employees:
         employees[(lastname, firstname)] = {}
@@ -51,16 +56,11 @@ for line in contacts_list[1:]:
         employees[(lastname, firstname)]['email'] = email
 
 
-pprint(employees)
-# '''
-# "[7|8]\s?\(?(?P<area>\d{3})\)?[\- ]?(?P<number>[\d\- ]{7,10})(?P<ext>\(?доб. \d{4}?\)?)?"gm
-# '''
-employees_list =[['lastname','firstname','surname','organization','position','phone','email'],]
+employees_list = [['lastname', 'firstname', 'surname', 'organization', 'position', 'phone', 'email']]
 for (lastname, firstname), employees_data in employees.items():
     employee_record = [lastname, firstname, employees_data['surname'], employees_data['organization'], employees_data.get('position'), employees_data['phone'], employees_data.get('email')]
     employees_list.append(employee_record)
 
 with open("phonebook.csv", "w") as f:
     datawriter = csv.writer(f, delimiter=',')
-    # Вместо contacts_list подставьте свой список
     datawriter.writerows(employees_list)
